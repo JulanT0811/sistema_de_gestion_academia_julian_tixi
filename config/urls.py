8a0e7753511475dcf3543cd2fadd1cf888e38941
JulanT0811 <jtlengua.9b@gmail.com>
2026-06-02 05:12:22 -0500

    cambio

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from catalog.views import CursoViewSet, EstudianteViewSet, CalcularNotasView, ProgramarModulosView
+from sistema_de_gestion_academia_julian_tixi.catalog.views import CursoViewSet, EstudianteViewSet, CalcularNotasView, ProgramarModulosView
 
 router = DefaultRouter()
 router.register(r'cursos', CursoViewSet)
